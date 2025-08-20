bebef8a7818cf8f983b4d3db603bc4db2f7b31bc
fifovalle <fifanaufal10@gmail.com>
2025-08-20 17:40:57 +0700

    feat: add rectangle transition

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Sometype_Mono } from "next/font/google";
 // Components
 import MainNav from "@/components/MainNav";
 import PageTransition from "@/components/PageTransition";
+import RectangleTransition from "@/components/RectangleTransition";
 
 const someTypeMono = Sometype_Mono({
     variable: "--font-sometypeMono",
@@ -22,7 +23,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${someTypeMono.variable} antialiased`}>
+            <body
+                className={`${someTypeMono.variable} antialiased overflow-hidden relative`}
+            >
+                {/* =============== RECTANGLES TRANSITION ================ */}
+                <RectangleTransition />
                 {/* =============== PAGE TRANSITION ================ */}
                 <PageTransition>
                     {/* =============== WRAPPER ================ */}
@@ -33,7 +38,7 @@ export default function RootLayout({
                         </div>
 
                         {/* =============== CONTENT ================ */}
-                        <div className="w-full max-w-[1130px] px-[15px] mx-auto bg-pink-50/10">
+                        <div className="w-full max-w-[1130px] px-[15px] mx-auto">
                             <header>header</header>
                             {children}
                         </div>
---
+++ constants/rectangleVariants.ts
@@ -0,0 +1,14 @@
+export const rectangleVariants = {
+    initial: {
+        y: "-100%",
+        height: "100%",
+    },
+    animate: {
+        y: "0%",
+        height: "0%",
+    },
+    exit: {
+        y: ["0%", "-100%"],
+        height: ["0%", "100%"],
+    },
+};
